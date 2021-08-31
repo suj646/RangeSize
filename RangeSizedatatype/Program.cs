d7de8ea6aa7f714b6cd5c0bb6d72639eca9277c7
Sujit Bagade <sujitbagade2@gmail.com>
2021-08-31 23:17:39 +0530

    RangeSize

---
+++ RangeSizedatatype/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace RangeSizedatatype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            bool boolVar = true; 
+            byte byteVar = 255; 
+            short shortVar = -66; 
+            ushort ushortVar = 66; 
+            int intVar = -1000; 
+            uint uintVar = 1000; 
+            long longVar = -56000;
+            ulong ulongVar = 56000; 
+            float floatVar = 160.54F; 
+            double doubleVar = 13782.57543D; 
+            char charVar = 'A';
+            Console.WriteLine("Value Data Types ");
+            Console.WriteLine(boolVar);
+            Console.WriteLine(byteVar);
+            Console.WriteLine(shortVar);
+            Console.WriteLine(ushortVar);
+            Console.WriteLine(intVar);
+            Console.WriteLine(uintVar);
+            Console.WriteLine(longVar);
+            Console.WriteLine(ulongVar);
+            Console.WriteLine(floatVar);
+            Console.WriteLine(doubleVar);
+            Console.WriteLine(charVar);
+        }
+    }
+}
+
